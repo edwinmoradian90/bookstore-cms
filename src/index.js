803283cc1569f925bcef4b5ee793d4f5c460fc17
Edwin Moradian <edwin.moradian90@gmail.com>
2020-08-16 21:20:11 +0400

    fixing all fixable linter errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import './index.css';
 import rootReducer from './reducers/index';
 import App from './components/App';
 
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,3 @@
-const generateID = () => {
-  return Math.floor(Math.random() * Math.floor(1));
-};
+const generateID = () => Math.floor(Math.random() * Math.floor(1));
 
 export default generateID;
